e48ea04c1b1c0e53c9d0f0a44a9e503381e995da
NatnaelZeleke <se.natnael.zeleke@gmail.com>
2020-05-29 16:03:49 +0300

    ++Added comment functionality

--- src/app/feature-modules/user/home-con/home-page/post/post.component.ts
+++ src/app/feature-modules/user/home-con/home-page/post/post.component.ts
@@ -5,6 +5,10 @@ import {IProfile} from '../../../../../api/models/profile.model';
 import {Account} from '../../../../../api/models/account.model';
 import {LikesService} from '../../../../../api/services/likes.service';
 import {ILikes} from '../../../../../api/models/likes.model';
+import {CommentService} from '../../../../../api/services/comment.service';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
+import {Comment, IComment} from '../../../../../api/models/comment.model';
+
 
 @Component({
   selector: 'app-post',
@@ -18,16 +22,31 @@ export class PostComponent implements OnInit {
   profile: IProfile[] = [];
   isLiked = false;
   likeObject: ILikes;
+  likeCount = 0;
+  commentCount = 0;
+  isCommentShown = false;
+  commentForm: FormGroup;
+  comments: IComment[] = [];
+  isCommentLoaded = false;
 
   constructor(public postService: ProfileService,
-              public likeService: LikesService) {
+              public likeService: LikesService,
+              public commentService: CommentService,
+              public formBuilder: FormBuilder) {
   }
 
   ngOnInit() {
+    this.getCommentCount();
     this.isPostLiked();
     this.loadProfilePicture();
-  }
+    this.getLikeCount();
 
+    this.commentForm = this.formBuilder.group(
+      {
+        comment: ['', Validators.required]
+      }
+    );
+  }
 
   loadProfilePicture() {
     this.postService.query({
@@ -43,7 +62,6 @@ export class PostComponent implements OnInit {
         }
       );
   }
-
 
   isPostLiked() {
     this.likeService.query({
@@ -62,8 +80,6 @@ export class PostComponent implements OnInit {
   like() {
     if (!this.isLiked) {
       this.isLiked = true;
-      // create like
-      // change status
       const like: ILikes = {
         postId: this.post.id,
         userLogin: this.account.login
@@ -71,23 +87,76 @@ export class PostComponent implements OnInit {
       this.likeService.create(like)
         .subscribe(result => {
           if (result.body != null) {
-
             this.likeObject = result.body;
           }
         });
-
     } else {
       this.likeService.delete(this.likeObject.id)
         .subscribe(result => {
-
         });
-      // delete like
-      // change status
       this.likeObject = null;
       this.isLiked = false;
 
     }
   }
 
+  getLikeCount() {
+    this.likeService.queryCount({
+      page: 0,
+      size: 1,
+      'postId.equals': this.post.id
+    }).subscribe(result => {
+      this.likeCount = result.body;
+    });
+  }
+
+  getCommentCount() {
+    this.commentService.queryCount({
+      page: 0,
+      size: 1,
+      'postId.equals': this.post.id
+    }).subscribe(result => {
+      this.commentCount = result.body;
+    });
+  }
+
+  toggleComment() {
+    this.isCommentShown = !this.isCommentShown;
+    if (!this.isCommentLoaded) {
+      this.isCommentLoaded = true;
+      this.getCommentList();
+    }
+  }
+
+  // comment control section
+
+  postComment() {
+    if (this.commentForm.valid) {
+      const com: Comment = {
+        content: this.commentForm.value.comment,
+        postId: this.post.id,
+        userLogin: this.account.login
+      };
+      this.commentService.create(com)
+        .subscribe(result => {
+          // if(result)
+        });
+    }
+
+  }
+
+  getCommentList() {
+    this.commentService.query({
+      page: 0,
+      size: 1,
+      'postId.equals': this.post.id
+    }).subscribe(result => {
+      this.comments = result.body;
+    });
+  }
+
+  get f() {
+    return this.commentForm.controls;
+  }
 
 }
